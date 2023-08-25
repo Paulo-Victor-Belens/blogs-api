e63feac213598c38469aac72d4f9ee92f93b754f
Paulo Victor Belens <paulovictor55296@gmail.com>
2023-08-25 19:46:08 -0300

    7º req concluido

---
+++ src/models/Category.js
@@ -0,0 +1,22 @@
+const CategorySchema = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, 
+  { 
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  return Category;
+};
+
+module.exports = CategorySchema;
